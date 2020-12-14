5f46a54cf4048f92f4d28e4555851f1216b0bf46
emmadrd912 <emma.durand@ynov.com>
2020-12-14 11:12:10 +0100

    maj name script

--- script_brute.py
+++ script_brute.py
@@ -11,25 +11,24 @@ start = timer()
 with open('shadow', 'r') as f:
    lines = f.readlines()
 
-md5_hash = []
 for line in lines:
     shadow_items = line.split(":")
     for items in shadow_items:
         if items.startswith("$1"):
             md5 = items.replace("$1$", "")
-            md5_hash.append(md5)
+            name = shadow_items[0]
 
-caractere = 'abcdefghijklmnopqrstuvwxyz;_@#ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-for current in xrange(5,11):
-    a = [i for i in caractere]
-    for y in xrange(current):
-        a = [x+i for i in caractere for x in a]
-    for data in a :
-        hash = hashlib.md5(data.encode()).hexdigest()
-        for mdp_shadow in md5_hash:
-            if str(hash) ==  str(mdp_shadow):
-                end = timer()
-                with open('password_decrypt_brute.txt', 'a') as d:
-                    temps = end-start
-                    d.write("Mot de passe : "+str(data)+", temps : "+str(temps)+" s \n")
-                    print("Mot de passe : " +str(data)+", temps : "+str(temps)+" s")
+            # caractere = 'abcdefghijklmnopqrstuvwxyz;_@#ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+            caractere = "brazil"
+            for current in xrange(5,11):
+                a = [i for i in caractere]
+                for y in xrange(current):
+                    a = [x+i for i in caractere for x in a]
+                for data in a :
+                    hash = hashlib.md5(data.encode()).hexdigest()
+                    if str(hash) ==  str(md5):
+                        end = timer()
+                        with open('password_decrypt_brute.txt', 'a') as d:
+                            temps = end-start
+                            d.write("User : "+ name + ", mot de passe : "+str(data)+", temps : "+str(temps)+" s \n")
+                            print("User : "+ name + ", mot de passe : " +str(data)+", temps : "+str(temps)+" s")
--- script_dico.py
+++ script_dico.py
@@ -8,24 +8,24 @@ from timeit import default_timer as timer
 with open('shadow', 'r') as f:
    lines = f.readlines()
 
-md5_hash = []
 for line in lines:
     shadow_items = line.split(":")
     for items in shadow_items:
         if items.startswith("$1"):
             md5 = items.replace("$1$", "")
-            md5_hash.append(md5)
+            name = shadow_items[0]
 
-with open('dico_mini_fr', 'r') as f:
-   lines = f.readlines()
+            with open('dico_mini_fr', 'r') as f:
+               lines = f.readlines()
 
-for line in lines:
-    start = timer()
-    hash = line.strip()
-    dico_hash = hashlib.md5(hash.encode()).hexdigest()
-    for mdp_shadow in md5_hash:
-        if str(dico_hash) == str(mdp_shadow):
-            end = timer()
-            with open('password_decrypt_dico.txt', 'a') as d:
-                temps = end-start
-                d.write("Mot de passe : "+str(line.strip())+", temps : "+str(temps)+" s \n")
+            for line in lines:
+                start = timer()
+                hash = line.strip()
+                dico_hash = hashlib.md5(hash.encode()).hexdigest()
+
+                if str(dico_hash) == str(md5):
+                    end = timer()
+                    with open('password_decrypt_dico.txt', 'a') as d:
+                        temps = end-start
+                        d.write("User : " + name + ", mot de passe : "+str(line.strip())+", temps : "+str(temps)+" s \n")
+                        print("User : " + name + ", mot de passe : "+str(line.strip())+", temps : "+str(temps)+" s")
